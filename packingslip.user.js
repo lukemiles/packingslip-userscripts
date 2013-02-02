d8fbab17147f0d5136421a625ad4c53d25c77225
Luke Miles <luke@lukemil.es>
2013-02-02 14:18:41 -0800

    0.1.b
    Initial version. Removes ship to, moves address in top left, removes
    seller info and finds and prints box number.

--- packingslip.user.js
+++ packingslip.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name       Invoice Corrector
 // @namespace  https://sellercentral.amazon.com
-// @version    0.1
+// @version    0.1b
 // @description  Fixes Amazon Invoices
-//  @match 		https://sellercentral.amazon.com/*
+// @match 		https://sellercentral.amazon.com/*
 // @copyright  2012+, You
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js
 // @require       http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.2/jquery-ui.min.js
@@ -12,5 +12,16 @@
 //remove ship to
 $("table:first").find("tr:first").remove();
 
+//move address
+$("table:first").css("margin-left","-21px").css("margin-top", "-27px");
+
 //remove everything after the info containter
 $('.printbucket').nextAll().remove();
+
+//regex to find box number
+var info = $('.printedsubtext:first').text();
+re = /\((.*)\)/i;
+var boxnumber = info.match(re)[1];
+
+//prints large box number
+$('body').append('<p id="boxnumber" style="font-size: 60px; text-align:center;">' + boxnumber + '</p>');
